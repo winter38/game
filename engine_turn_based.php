ff2121be3a78daf3f1dddfdd0a4f13c66444a25c
Winter <winter38@inbox.lv>
2014-10-23 16:10:22 +0300

    Update engine_turn_based.php

--- engine_turn_based.php
+++ engine_turn_based.php
@@ -1,5 +1,13 @@
-<?php
+1. make live battle structure (include battlefield envirment, user structure,)
+2. Structure will be serialized (only of battle, other must be in table) 
+3. Each time page called - will draw battle from serialized structure
+4. Processors of battle will update structure
 
 
-
-?>
+battle
+  id
+  current_round
+  state
+  player  - player structure with current states
+  enemies - array with enemies structures
+  
